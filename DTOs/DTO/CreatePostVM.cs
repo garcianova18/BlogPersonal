9d37256899813eafee63dea1b0699b8a0cf518a0
wilson18_laptop <wilsongarcianova18@gmail.com>
2023-05-19 04:52:35 -0400

    se hicieron ajuste en el diseno

--- DTOs/DTO/CreatePostVM.cs
+++ DTOs/DTO/CreatePostVM.cs
@@ -9,14 +9,18 @@ namespace DTOs.DTO
     {
 
         [Required (ErrorMessage ="El Titulo es obligatorio")]
+        [Display(Name = "Título")]
         public string Titulo { get; set; }
 
         [Required(ErrorMessage = "La descripcion es obligatoria")]
+        [Display(Name = "Descripción")]
         public string Descripcion { get; set; }
 
+        [Display(Name ="Foto")]
         public IFormFile ImagenFile { get; set; }
 
-        [Required(ErrorMessage = "Debes elegir una Categorio")] 
+        [Required(ErrorMessage = "Debes elegir una Categorio")]
+        [Display(Name = "Categoría")]
         public int IdCategoria { get; set; }
 
 
